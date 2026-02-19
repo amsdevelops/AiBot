597f57c142b17fa148940b06ba9a94c5f0926cf2
amsdevelops <amsdevelops@gmail.com>
2026-02-19 20:54:36 +0300

    added temperature support

--- src/main/kotlin/ai/bot/app/remote/model/OpenAIRequest.kt
+++ src/main/kotlin/ai/bot/app/remote/model/OpenAIRequest.kt
@@ -6,5 +6,6 @@ data class OpenAIRequest(
     @SerializedName("model") val model: String,
     @SerializedName("input") val input: String,
     @SerializedName("previous_response_id") val previousResponseId: String? = null,
+    @SerializedName("temperature") val temperature: Double,
     @SerializedName("store") val store: Boolean = false
 )
